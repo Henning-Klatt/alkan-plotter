790368ffe4ad6d3366d75b0009ed0c49f4026d7c
Henning Klatt <Henning@HP-260-6G.fritz.box>
2017-10-20 21:31:45 +0200

    Twitter integration

---
+++ python/twitter.py
@@ -0,0 +1,8 @@
+import tweepy
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+api.update_status("Erster API Tweet?")
