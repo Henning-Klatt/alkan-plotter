1693799cc27be2207cb2dfee2a63f5b762d8b049
Henning Klatt <Henning@HP-260-6G.fritz.box>
2017-10-21 00:56:21 +0200

    rotation jeah

--- python/plot.py
+++ python/plot.py
@@ -3,6 +3,9 @@ from vapory import *
 from moviepy.editor import VideoClip
 import json
 import tweepy
+import numpy as np
+import math
+from math import pi
 from credentials import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -12,21 +15,48 @@ api = tweepy.API(auth)
 with open('data.json') as data_file:
     data = json.load(data_file)
 
+lange = 2 #sek. Länge der gif !!! must be int !!!
 obj = []
+x_cam = []
+y_cam = []
+z_cam = []
+x_max = [0]
+x_min = [0]
+y_max = [0]
+y_min = [0]
 
-def make_scene(t, x):
-    camera = Camera( 'location', [20,20,-40], 'look_at', [0,0,0])
+def make_scene(t):
+    #camera = Camera( 'location', [t*50,20,-((x_max[0]+y_max[0])/1.25)], 'look_at', [x_max[0]/2,0,0])
+    t = int(t*25)
+    camera = Camera( 'location', [x_cam[t],y_cam[t],z_cam[t]], 'look_at', center)
     return Scene( camera, objects= obj, included=["glass.inc"])
 
 def make_frame(t):
     #return scene.render("test.png", width=1024, height=512, antialiasing = 0.01, quality=100)
-    return make_scene(t, 1).render(width=1024, height=512, antialiasing = 0.01, quality=100)
+    return make_scene(t).render(width=1024, height=512, antialiasing = 0.01, quality=100)
+
+def generate_circle(center=(0,0,0), r=50, n=100):
+    return [
+        (
+            center[0] + (math.cos(2 * pi / n * x) * r),  # x
+            center[1] + (math.sin(2 * pi / n * x) * r),  # z
+            center[2]# + (math.sin(2 * pi / n * x) * r)   # y
+
+        ) for x in range(0, n + 1)]
 
 def render(data, length, shrink, empty, x, y, z):
     if isinstance(data, list):
         obj.append(Sphere([x, y, z], 2, Texture(Pigment('color', [0.1,0.1,0.1])))) # Create C node
         for i,item in enumerate(data): # Create New nodes around C
             [x_new, y_new, len_new, empty_new] = direct(len(data), length, shrink, empty, i, x, y, z)
+            if(x_new > x_max[0]):
+                x_max[0] = x_new
+            if(x_new < x_min[0]):
+                x_min[0] = x_new
+            if(y_new > y_max[0]):
+                y_max[0] = y_new
+            if(y_new < y_min[0]):
+                y_min[0] = y_new
             obj.append(Cylinder([x,y,z], render(item, len_new, shrink, empty_new, x_new, y_new, z), 0.3, Finish('ambient', 0.1, 'diffuse', 0.7), Pigment('color', [0.4,0.4,0.4])))
     elif data == 'h': # Create new h node
         obj.append(Sphere([x, y, z], 2, Texture(Pigment('color', [0.9,0.9,0.9]))))#White color
@@ -54,9 +84,19 @@ def direct(datasize, length, shrink, empty, i, x, y, z):
     return [x_new, y_new, len_new, empty]
 
 render(data, 15, 10, -1, 0, 0, 0)
+print("X max: " + str(x_max[0]) + ", Y max: " + str(y_max[0]))
+print("Kreispunkte: " + str(lange*25+1))
+center = ((x_max[0]+x_min[0])/2, (y_max[0]+y_min[0])/2, 0)
+
+print("Kreismitte: " + str(center))
+points = generate_circle(center=center, r=(x_max[0]+y_max[0]), n=lange*25+1)
+for x,y,z in points:
+    x_cam.append(x)
+    y_cam.append(z)
+    z_cam.append(y)
 
 obj.append(LightSource( [10, 120, -40], 'color', [1.3, 1.3, 1.3]))
 obj.append(Background("color", [1,1,1]))
 
-VideoClip(make_frame, duration=1).write_gif("anim.gif",fps=1)
-api.update_with_media("anim.gif", "Haskell Foreign Function Interface fehlt. lol.")
+VideoClip(make_frame, duration=lange).write_gif("animation.gif",fps=25)
+#api.update_with_media("animation.gif", "Haskell Foreign Function Interface fehlt. lol.")
